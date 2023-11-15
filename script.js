380f6f72d9c156ce5bf89aab21d43f92a235b238
James <orangeivern29@gmail.com>
2023-11-15 16:02:49 +0800

    feat added a feature where it validates the user input if its in the choices

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let getUserChoice = prompt(" Pick one!:Rock,Paper,Scissor");
+let getUserChoice = userConvertedChoice(prompt(" Pick one!:Rock,Paper,Scissor"));
 let getComputerChoice =  randomizer()
-
+let gameLevels = prompt("How Many rounds do you want?")
 
 playGame(getUserChoice,getComputerChoice, showResults)
+
 
 
 
@@ -10,7 +11,6 @@ function playGame(getUserChoice,getComputerChoice,callback)
 {
 	callback(getUserChoice,getComputerChoice)
 }
-
 
 function showResults(user,computer)
 {
@@ -24,14 +24,44 @@ function showResults(user,computer)
  {
  	if (user.length === 4)
  	{
- 		console.log("hello")
+ 	  if(user.length < computer.length && computer.length == 5)
+ 	  {
+ 	  	console.log(`${computer} beats ${user}, You Lose!`)
+ 	  }
+ 	  else
+ 	  {
+ 	  	console.log(`${user} beats ${computer}, You Win!`)
+ 	  }
+ 	}
+ 	if (user.length === 5)
+ 	{
+ 	  if(user.length < computer.length && computer.length == 7)
+ 	  {
+ 	  	console.log(`${computer} beats ${user}, You Lose!`)
+ 	  }
+ 	  else
+ 	  {
+ 	  	console.log(`${user} beats ${computer}, You Win!`)
+ 	  }
+ 	}
+ 	if (user.length === 7)
+ 	{
+ 	  if(user.length > computer.length && computer.length == 4)
+ 	  {
+ 	  	console.log(`${computer} beats ${user}, You Lose!`)
+ 	  }
+ 	  else
+ 	  {
+ 	  	console.log(`${user} beats ${computer}, You Win!`)
+ 	  }
  	}
  }
+ 
 }  
 
 
 
-
+//Returns Computer's random pick in string
 function randomizer()
 {
 	let random = Math.floor(Math.random()*3)+1
@@ -46,5 +76,25 @@ function randomizer()
 	else
 	{
 		return "Scissor"
+	}
+}
+//Returns User's converted input into formal String
+function userConvertedChoice(pick)
+{
+	if(pick.length === 4)
+	{
+		return "Rock"
+	}
+	else if(pick.length === 5)
+	{
+		return "Paper"
+	}
+	else if (pick.length === 7)
+	{
+		return "Scissor"
+	}
+	else
+	{
+		alert(`${pick} is not in the options -_-`)
 	}
 }
